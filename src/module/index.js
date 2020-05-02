be955e4b284e9f8e8f49b4f5b8038d20ea6e9bf8
Daniel Laloush <d.laloush@outlook.com>
2020-05-02 15:42:51 -0400

    added finish building preliminar website

--- src/module/index.js
+++ src/module/index.js
@@ -1,3 +1,31 @@
 import '../stylesheet/main.scss';
 
-console.log('Hello World!');
+const bgImg = () => {
+  const div = document.createElement('div');
+  div.className = 'main';
+  const logo = document.createElement('img');
+  logo.src = '../src/media/logo.jpg';
+  const construction = document.createElement('img');
+  construction.src = '../src/media/construction.jpg';
+  div.appendChild(logo);
+  div.appendChild(construction);
+
+  const divMedia = document.createElement('div');
+  divMedia.className = 'media';
+  const fb = document.createElement('a');
+  fb.innerHTML = '<i class="fab fa-facebook"></i>';
+  fb.href = 'https://www.facebook.com/ndmlawfirm/';
+  const ig = document.createElement('a');
+  ig.innerHTML = '<i class="fab fa-instagram"></i>';
+  ig.href = 'https://www.instagram.com/ndmlawfirm/';
+
+  divMedia.appendChild(fb);
+  divMedia.appendChild(ig);
+
+  document.body.appendChild(div);
+  document.body.appendChild(divMedia);
+};
+
+window.onload = () => {
+  bgImg();
+};
