6e3cfa51a473a9df44eb1cd86b44c55998006eb7
Daniel Laloush <d.laloush@outlook.com>
2020-05-03 09:39:45 -0400

    deleted commented files

--- src/module/vendor/fontawesome.js
+++ src/module/vendor/fontawesome.js
@@ -1,6 +1,5 @@
 import '@fortawesome/fontawesome-free/js/all';
 
-/* import '@fortawesome/fontawesome-free/js/light'; */
 import '@fortawesome/fontawesome-free/js/regular';
 import '@fortawesome/fontawesome-free/js/solid';
 import '@fortawesome/fontawesome-free/js/brands';
