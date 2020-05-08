57ef3e89f21b1cfa286bfad6740b5511cb5f768e
Daniel Laloush <d.laloush@outlook.com>
2020-05-08 19:51:06 -0400

    added page structure

--- src/module/main.js
+++ src/module/main.js
@@ -1,34 +1,24 @@
-const bgImg = () => {
-  const div = document.createElement('div');
-  div.className = 'main';
-  const logo = document.createElement('img');
-  logo.src = '../src/media/logo.jpg';
-  const construction = document.createElement('img');
-  construction.src = '../src/media/construction.jpg';
-  div.appendChild(logo);
-  div.appendChild(construction);
+const page = (element) => document.createElement(element);
+let body;
 
-  const divMedia = document.createElement('div');
-  divMedia.className = 'media';
-  const fb = document.createElement('a');
-  fb.innerHTML = '<i class="fab fa-facebook"></i>';
-  fb.href = 'https://www.facebook.com/ndmlawfirm/';
-  const ig = document.createElement('a');
-  ig.innerHTML = '<i class="fab fa-instagram"></i>';
-  ig.href = 'https://www.instagram.com/ndmlawfirm/';
+const header = () => {
+  const header = page('header');
+  body.appendChild(header);
+};
 
-  divMedia.appendChild(fb);
-  divMedia.appendChild(ig);
+const main = () => {
+  const main = page('main');
+  body.appendChild(main);
+};
 
-  const heroImg = document.createElement('img');
-  heroImg.className = 'hero-img';
-  heroImg.src = '../src/media/DanielLogo.png';
-
-  document.body.appendChild(heroImg);
-  document.body.appendChild(div);
-  document.body.appendChild(divMedia);
+const footer = () => {
+  const footer = page('footer');
+  body.appendChild(footer);
 };
 
 window.onload = () => {
-  bgImg();
+  body = document.body;
+  header();
+  main();
+  footer();
 };
