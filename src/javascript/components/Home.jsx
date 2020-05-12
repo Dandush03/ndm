ed54a7d9c58e8633b95d99446225e6d0c820f222
Daniel Laloush <d.laloush@outlook.com>
2020-05-12 19:51:11 -0400

    adding header

--- src/javascript/components/Home.jsx
+++ src/javascript/components/Home.jsx
@@ -4,6 +4,61 @@ import React, { Component } from 'react';
 import { TimingSlides, ShowSlides } from '../slide-show';
 import Content from '../api';
 
+
+const HeaderSection = (data) => {
+  const content = [];
+  if (data.text !== null && data.text !== undefined) {
+    content.push(
+      <div>
+        <div>
+          <div className="brand-info">
+            <p>Paitilla</p>
+            <p>info@ndm.com.pa</p>
+          </div>
+          <div className="media-icons">
+            <icons />
+            <icons />
+            <icons />
+          </div>
+        </div>
+        <div className="menu">
+          <div className="brand-logo">
+            <img src="./media/logo.png" alt="Company Logo" className="brand-logo" />
+          </div>
+          <div>
+            <ul>
+              <li><button type="button">{data.text.home}</button></li>
+              <li><button type="button">{data.text.about}</button></li>
+              <li><button type="button">{data.text.work}</button></li>
+              <li><button type="button">{data.text.contact}</button></li>
+            </ul>
+            <p>800.800.8000</p>
+          </div>
+        </div>
+      </div>,
+    );
+  }
+  return (
+    content
+  );
+};
+
+const TopSection = (data) => {
+  const content = [];
+  if (data.text !== null && data.text !== undefined) {
+    content.push(
+      <div className="content">
+        <span className="span">{data.text.span}</span>
+        <h2>{data.text.title}</h2>
+        <button type="button">{data.text.btn}</button>
+      </div>,
+    );
+  }
+  return (
+    content
+  );
+};
+
 const SlideSection = () => (
   <div className="slideshow-container">
     <div className="my-slides fade">
@@ -52,22 +107,6 @@ const BtnsSection = (data) => {
   );
 };
 
-const TopSection = (data) => {
-  const content = [];
-  if (data.text !== null && data.text !== undefined) {
-    content.push(
-      <div className="content">
-        <span>{data.text.span}</span>
-        <h2>{data.text.title}</h2>
-        <button type="button">{data.text.btn}</button>
-      </div>,
-    );
-  }
-  return (
-    content
-  );
-};
-
 const IntroSection = (data) => {
   const content = [];
   if (data.content !== null && data.content !== undefined) {
@@ -77,11 +116,13 @@ const IntroSection = (data) => {
           <h2>{data.content.title}</h2>
         </div>
         <div>
-          <h4>{data.content.firstLetter}</h4>
-          <span>{data.content.firstContent}</span>
+          <div className="mid">
+            <h4>{data.content.firstLetter}</h4>
+            <p>{data.content.firstContent}</p>
+          </div>
         </div>
         <div>
-          <span>{data.content.secondContent}</span>
+          <p>{data.content.secondContent}</p>
         </div>
       </div>,
     );
@@ -109,21 +150,24 @@ export default class Home extends Component {
     let btns;
     let topSection;
     let introSection;
+    let menu;
     if (data !== undefined) {
       btns = data.btns;
       topSection = data.topSection;
       introSection = data.introSection;
+      menu = data.menu;
     }
     return (
       <div className="main-container">
         <img src="https://res.cloudinary.com/dl-cultures/image/upload/v1588615344/logo/HeroLogo.svg" alt="Hero Logo" className="hero-logo" />
-        <header />
+        <header>
+          <HeaderSection text={menu} />
+        </header>
         <main>
           <section className="top-section">
-            <img src="./media/logo.png" alt="Company Logo" className="brand-logo" />
             <SlideSection />
             <TopSection text={topSection} />
-            <BtnsSection btns={btns || null} />
+            <BtnsSection btns={btns} />
           </section>
           <section className="intro-section">
             <IntroSection content={introSection} />
