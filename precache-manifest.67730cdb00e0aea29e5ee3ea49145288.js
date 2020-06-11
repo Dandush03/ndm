8ec957a14fc2ebbee16bdf1dd3db8e248a6c8f76
Daniel Laloush <d.laloush@outlook.com>
2020-06-10 20:14:13 -0400

    Updates

---
+++ precache-manifest.67730cdb00e0aea29e5ee3ea49145288.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a48c1274c8bd0c7d6d6b0d11ec38617",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ccf4f0977d71411c9e1b",
+    "url": "/static/css/main.b3ddf69b.chunk.css"
+  },
+  {
+    "revision": "0b757f78f124025ccd1a",
+    "url": "/static/js/2.35d68b6a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.35d68b6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccf4f0977d71411c9e1b",
+    "url": "/static/js/main.880ba56b.chunk.js"
+  },
+  {
+    "revision": "220ccc44546e8a1c2fc1",
+    "url": "/static/js/runtime-main.808b8eee.js"
+  },
+  {
+    "revision": "e29266125d4df3c616ea721682254f2d",
+    "url": "/static/media/GildaDisplay-Regular.e2926612.ttf"
+  },
+  {
+    "revision": "0d5f74db34aab37da60d2e18e443d2e1",
+    "url": "/static/media/SuezOne-Regular.0d5f74db.ttf"
+  },
+  {
+    "revision": "609707bbc156d9cfa5ce79fb66b6e222",
+    "url": "/static/media/gallery_01.609707bb.jpg"
+  },
+  {
+    "revision": "28749e8212cc6344e6c519be31a45771",
+    "url": "/static/media/gallery_02.28749e82.jpg"
+  },
+  {
+    "revision": "cb0b212ba0af7a5d723f47e385feebb7",
+    "url": "/static/media/info.cb0b212b.png"
+  },
+  {
+    "revision": "c91343f2987959114ab1a725ea5004c6",
+    "url": "/static/media/list-type.c91343f2.svg"
+  },
+  {
+    "revision": "9715e2397a5fe491580f6ca2198ac79d",
+    "url": "/static/media/ndm-english.9715e239.mp4"
+  },
+  {
+    "revision": "cb1b4c5d993963ddc3caebf0da996875",
+    "url": "/static/media/ndm-spanish.cb1b4c5d.mp4"
+  },
+  {
+    "revision": "694ba6763bfa08a70545afe9257cd3a3",
+    "url": "/static/media/panama1.694ba676.png"
+  },
+  {
+    "revision": "2444d6b0b9f5ea7433d81cb4edabcb72",
+    "url": "/static/media/panama2.2444d6b0.jpg"
+  },
+  {
+    "revision": "ff15a53ef9884fac0f5773b85fa83720",
+    "url": "/static/media/panama3.ff15a53e.jpg"
+  },
+  {
+    "revision": "8a4bb5b2ca4f8bfcfc7479428fd2dbe1",
+    "url": "/static/media/panama4.8a4bb5b2.jpg"
+  },
+  {
+    "revision": "761b09433fdd64ac0f60d326902e489c",
+    "url": "/static/media/quote.761b0943.jpg"
+  },
+  {
+    "revision": "ea5e2552a894dd8c467ba647b82291f2",
+    "url": "/static/media/sub-hero.ea5e2552.jpg"
+  }
+]);
